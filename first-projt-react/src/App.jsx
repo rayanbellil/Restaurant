790261e48e25df4d79e6153ce3a61a817dfc2e50
Rayan <rayanbellill1infos4grp2@gmail.com>
2025-11-28 17:00:55 +0100

    update

---
+++ first-projt-react/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl">hello world </h1>
+      <div className="w-40 h-40 bg-red-400"></div>
+
+    </>
+  )
+}
+
+export default App
